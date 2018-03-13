86a121cfd96913b62bb6c0c120763162093bd33a
mflehmig <martin.flehmig@tu-dresden.de>
2018-03-13 14:54:44 +0100

    Add license text

--- src/dreadMM.c
+++ src/dreadMM.c
@@ -1,24 +1,46 @@
-/*! \file
- Copyright (c) 2003, The Regents of the University of California, through
- Lawrence Berkeley National Laboratory (subject to receipt of any required
- approvals from U.S. Dept. of Energy)
+/*
+Original work Copyright (c) 2003, The Regents of the University of California,
+through Lawrence Berkeley National Laboratory (subject to receipt of any required
+approvals from U.S. Dept. of Energy)
 
- All rights reserved.
+Modified work Copyright 2018 Technische Universität Dresden, Germany
 
- The source code is distributed under BSD license, see the file License.txt
- at the top-level directory.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+(1) Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+(2) Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+(3) Neither the name of Lawrence Berkeley National Laboratory, U.S. Dept. of
+Energy nor the names of its contributors may be used to endorse or promote
+products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+ * This file is part of SuperLUbench (https://github.com/mflehmig/SuperLUbench.git)
  */
 
-/*! @file 
- * \brief 
- * Contributed by Francois-Henry Rouet.
- *
- */
+
 #include "dreadMM.h"
 
 #include <ctype.h>
 
-//MS #include "superlu_ddefs.h"
 #include "slu_ddefs.h"
 
 #undef EXPAND_SYM
